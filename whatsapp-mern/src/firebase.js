834779a64ca6586ef428d8f46127cdef627bc1fd
Alfonso <alfonsodelag1@gmail.com>
2020-09-18 23:01:47 +0200

    adding main folder

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_7FaMfRv3r9T6jCcBSR1Irdw0-51m5UU",
+  authDomain: "whatsapp-mern-bbe12.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-bbe12.firebaseio.com",
+  projectId: "whatsapp-mern-bbe12",
+  storageBucket: "whatsapp-mern-bbe12.appspot.com",
+  messagingSenderId: "555484549134",
+  appId: "1:555484549134:web:9e0aa6fd142676cc0aac5f"
+};
